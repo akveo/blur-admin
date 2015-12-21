1e027c0f1c469ef1ddb6649eb20f73406e2f097b
alex <demeshko.alexander.v@gmail.com>
2015-12-21 12:38:33 +0300

    stopable interval for animations

---
+++ src/app/theme/services/stopableInterval.js
@@ -0,0 +1,16 @@
+/**
+ * @author a.demeshko
+ * created on 12/21/15
+ */
+(function () {
+  'use strict';
+
+  angular.module('theme')
+    .service('stopableInterval', stopableInterval);
+
+  /** @ngInject */
+  function stopableInterval() {
+
+  }
+
+})();
