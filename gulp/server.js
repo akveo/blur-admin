752bb8e48a1fdc3eb2d3eb1dceb4350a1813c15e
Vladimir Lugovsky <lugovsky.v@gmail.com>
2015-12-22 19:20:05 +0300

    feat(tabs): Tabs and accordion

--- gulp/server.js
+++ gulp/server.js
@@ -38,7 +38,8 @@ function browserSyncInit(baseDir, browser) {
   browserSync.instance = browserSync.init({
     startPath: '/',
     server: server,
-    browser: browser
+    browser: browser,
+    ghostMode: false
   });
 }
 
