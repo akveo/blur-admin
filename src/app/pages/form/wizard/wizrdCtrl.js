9440d873a9d966d9a0e887e1e89276321ee022dc
smartapant <smartapant@gmail.com>
2015-12-24 19:20:58 +0300

    Tabs directives begin.

--- src/app/pages/form/wizard/wizrdCtrl.js
+++ src/app/pages/form/wizard/wizrdCtrl.js
@@ -51,5 +51,57 @@
     $scope.progress = 0;
   }
 
+  function baWizard() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      templateUrl: 'app/pages/form/wizard/wizard2.html',
+      controller: ['$scope', function ($scope) {
+        var vm = this;
+
+        $scope.tabs = [];
+
+        $scope.tab = 0;
+        $scope.progress = 0;
+
+        $scope.$watch('tab', countProgress);
+
+        $scope.selectTab = function (tab) {
+          $scope.tab = tab;
+        };
+
+        $scope.isSelectedTab = function (tab) {
+          return $scope.tab === tab;
+        };
+
+        $scope.isFirstTab = function () {
+          return $scope.tab == 0;
+        };
+
+        $scope.isLastTab = function () {
+          return $scope.tab == $scope.tabs.length - 1 ;
+        };
+
+        $scope.nextTab = function () {
+          $scope.tab++;
+        };
+
+        vm.addTab = function(tab) {
+          
+        };
+
+        $scope.previousTab = function () {
+          $scope.tab--;
+        };
+
+        function countProgress() {
+          $scope.progress = (($scope.tab + 1) / $scope.tabs.length) * 100;
+        }
+
+      }]
+    }
+  }
+
 })();
 
