b211a4798be5fad130d8135c1aa50ab97482b2b2
Vladimir Lugovsky <lugovsky.v@gmail.com>
2016-01-05 13:42:08 +0300

    refactor(panel): refactor global panel styles

--- src/app/theme/components/baPanel/baPanel.service.js
+++ src/app/theme/components/baPanel/baPanel.service.js
@@ -16,7 +16,7 @@
       restrict: 'A',
       transclude: true,
       template: function(elem, attrs) {
-        var res = '<div class="panel-body"><div class="panel-content" ng-transclude></div></div>';
+        var res = '<div class="panel-body" ng-transclude></div>';
         if (attrs.baPanelTitle) {
           var titleTpl = '<div class="panel-heading clearfix"><h3 class="panel-title">' + attrs.baPanelTitle + '</h3></div>';
           res = titleTpl + res; // title should be before
