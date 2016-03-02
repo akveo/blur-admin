6198770b9c183b0221e1908b670a20567c7a2539
alex <demeshko.alexander.v@gmail.com>
2016-03-02 14:03:23 +0300

    feature(marketplace) back

---
+++ gulp/marketplace.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+var prompt = require('gulp-prompt');
+var rename = require('gulp-rename');
+
+gulp.task('marketplace-release', ['build'], function () {
+  return gulp.src('')
+    .pipe(prompt.prompt({
+      type: 'input',
+      name: 'version',
+      message: 'Please enter release version (x.x.x)'
+    }, function (res) {
+      var nameAndVersion = 'blur-admin-' + res.version;
+      return gulp
+        .src(['src/**', 'release/**', 'gulp/**', 'bower.json', 'gulpfile.js', 'package.json', 'README.md', '.gitignore'], {base: "."})
+        .pipe(rename(function (path) {
+          path.dirname = nameAndVersion + '/' + path.dirname;
+        }))
+        .pipe(zip(nameAndVersion + '.zip'))
+        .pipe(gulp.dest('.'));
+    }));
+
+});
