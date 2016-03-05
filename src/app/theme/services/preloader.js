f8cae976cff169462e2f80255a3bea214399839a
alex <demeshko.alexander.v@gmail.com>
2016-03-05 17:46:49 +0300

    refactor(litleIssues): preloader logs and firefox, tabs - icons,message-center icons raonded

--- src/app/theme/services/preloader.js
+++ src/app/theme/services/preloader.js
@@ -16,16 +16,13 @@
         var img = new Image();
         img.src = src;
         img.onload = function(){
-          console.log("img " + src + " loaded");
           d.resolve();
         };
         return d.promise;
       },
       loadAmCharts : function(){
         var d = $q.defer();
-        console.log("amchart");
         AmCharts.ready(function(){
-          console.log("amchart loaded");
           d.resolve();
         });
         return d.promise;
