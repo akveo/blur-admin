c424ead8371b11f913f2306721ad7cfa7e9bfc1d
Vladimir Lugovsky <lugovsky.v@gmail.com>
2016-04-22 21:05:41 +0300

    fix(selectpicker): make possible to use selectpicker with ngOptions Closes #18

--- src/app/pages/form/inputs/widgets/select/selectpicker.directive.js
+++ src/app/pages/form/inputs/widgets/select/selectpicker.directive.js
@@ -12,10 +12,27 @@
   function selectpicker() {
     return {
       restrict: 'A',
-      link: function( $scope, elem) {
-        setTimeout(function() {
-          elem.selectpicker({dropupAuto: false});
-        }, 0);
+      require: '?ngOptions',
+      priority: 1500, // make priority bigger than ngOptions and ngRepeat
+      link: {
+        pre: function(scope, elem, attrs) {
+          elem.append('<option data-hidden="true" disabled value="">' + (attrs.title || 'Select something') + '</option>')
+        },
+        post: function(scope, elem, attrs) {
+          function refresh() {
+            elem.selectpicker('refresh');
+          }
+
+          if (attrs.ngModel) {
+            scope.$watch(attrs.ngModel, refresh);
+          }
+
+          if (attrs.ngDisabled) {
+            scope.$watch(attrs.ngDisabled, refresh);
+          }
+
+          elem.selectpicker({ dropupAuto: false, hideDisabled: true });
+        }
       }
     };
   }
