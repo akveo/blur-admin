d849b044f5403b365f72b6e640dd43373f9d3643
Vladimir Lugovsky <lugovsky.v@gmail.com>
2016-05-05 17:27:39 +0300

    refactor(sidebar): static items configuration moved to provider config

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -20,8 +20,43 @@
       .config(routeConfig);
 
   /** @ngInject */
-  function routeConfig($urlRouterProvider) {
+  function routeConfig($urlRouterProvider, baSidebarServiceProvider) {
     $urlRouterProvider.otherwise('/dashboard');
+
+    baSidebarServiceProvider.addStaticItem({
+      title: 'Pages',
+      icon: 'ion-document',
+      subMenu: [{
+        title: 'Sign In',
+        fixedHref: 'auth.html',
+        blank: true
+      }, {
+        title: 'Sign Up',
+        fixedHref: 'reg.html',
+        blank: true
+      }, {
+        title: 'User Profile',
+        stateRef: 'profile'
+      }, {
+        title: '404 Page',
+        fixedHref: '404.html',
+        blank: true
+      }]
+    });
+    baSidebarServiceProvider.addStaticItem({
+      title: 'Menu Level 1',
+      icon: 'ion-ios-more',
+      subMenu: [{
+        title: 'Menu Level 1.1',
+        disabled: true
+      }, {
+        title: 'Menu Level 1.2',
+        subMenu: [{
+          title: 'Menu Level 1.2.1',
+          disabled: true
+        }]
+      }]
+    });
   }
 
 })();
--- src/app/theme/components/baSidebar/baSidebar.service.js
+++ src/app/theme/components/baSidebar/baSidebar.service.js
@@ -2,107 +2,85 @@
   'use strict';
 
   angular.module('BlurAdmin.theme.components')
-      .service('baSidebarService', baSidebarService);
+      .provider('baSidebarService', baSidebarServiceProvider);
 
   /** @ngInject */
-  function baSidebarService($state, layoutSizes) {
+  function baSidebarServiceProvider() {
+    var staticMenuItems = [];
 
-    var isMenuCollapsed = shouldMenuBeCollapsed();
-
-    var staticMenuItems = [ {
-        title: 'Pages',
-        icon: 'ion-document',
-        subMenu: [{
-            title: 'Sign In',
-            root: 'auth.html',
-            blank: true
-          }, {
-            title: 'Sign Up',
-            root: 'reg.html',
-            blank: true
-          }, {
-            title: 'User Profile',
-            root: '#/profile'
-          }, {
-            title: '404 Page',
-            root: '404.html',
-            blank: true
-          }]
-      }, {
-        title: 'Menu Level 1',
-        icon: 'ion-ios-more',
-        subMenu: [{
-            title: 'Menu Level 1.1',
-            root: '#',
-            disabled: true
-          }, {
-            title: 'Menu Level 1.2',
-            subMenu: [{
-              title: 'Menu Level 1.2.1',
-              root: '#',
-              disabled: true
-            }]
-          }]
-      }];
-
-    this.getMenuItems = function() {
-      var states = defineMenuItemStates();
-      var menuItems = states.filter(function(item) {
-        return item.level == 0;
-      });
-
-      menuItems.forEach(function(item) {
-        var children = states.filter(function(child) {
-          return child.level == 1 && child.name.indexOf(item.name) === 0;
-        });
-        item.subMenu = children.length ? children : null;
-      });
-
-      return menuItems.concat(staticMenuItems);
+    this.addStaticItem = function() {
+      staticMenuItems.push.apply(staticMenuItems, arguments);
     };
 
-    this.shouldMenuBeCollapsed = shouldMenuBeCollapsed;
-    this.canSidebarBeHidden = canSidebarBeHidden;
+    /** @ngInject */
+    this.$get = function($state, layoutSizes) {
+      return new _factory();
 
-    this.setMenuCollapsed = function(isCollapsed) {
-      isMenuCollapsed = isCollapsed;
-    };
+      function _factory() {
+        var isMenuCollapsed = shouldMenuBeCollapsed();
 
-    this.isMenuCollapsed = function() {
-      return isMenuCollapsed;
-    };
-
-    this.toggleMenuCollapsed = function() {
-      isMenuCollapsed = !isMenuCollapsed;
-    };
-
-    function defineMenuItemStates() {
-      return $state.get()
-          .filter(function(s) {
-            return s.sidebarMeta;
-          })
-          .map(function(s) {
-            var meta = s.sidebarMeta;
-            return {
-              name: s.name,
-              title: s.title,
-              level: (s.name.match(/\./g) || []).length,
-              order: meta.order,
-              icon: meta.icon,
-              stateRef: s.name,
-            };
-          })
-          .sort(function(a, b) {
-            return (a.level - b.level) * 100 + a.order - b.order;
+        this.getMenuItems = function() {
+          var states = defineMenuItemStates();
+          var menuItems = states.filter(function(item) {
+            return item.level == 0;
           });
-    }
 
-    function shouldMenuBeCollapsed() {
-      return window.innerWidth <= layoutSizes.resWidthCollapseSidebar;
-    }
+          menuItems.forEach(function(item) {
+            var children = states.filter(function(child) {
+              return child.level == 1 && child.name.indexOf(item.name) === 0;
+            });
+            item.subMenu = children.length ? children : null;
+          });
 
-    function canSidebarBeHidden() {
-      return window.innerWidth <= layoutSizes.resWidthHideSidebar;
-    }
+          return menuItems.concat(staticMenuItems);
+        };
+
+        this.shouldMenuBeCollapsed = shouldMenuBeCollapsed;
+        this.canSidebarBeHidden = canSidebarBeHidden;
+
+        this.setMenuCollapsed = function(isCollapsed) {
+          isMenuCollapsed = isCollapsed;
+        };
+
+        this.isMenuCollapsed = function() {
+          return isMenuCollapsed;
+        };
+
+        this.toggleMenuCollapsed = function() {
+          isMenuCollapsed = !isMenuCollapsed;
+        };
+
+        function defineMenuItemStates() {
+          return $state.get()
+              .filter(function(s) {
+                return s.sidebarMeta;
+              })
+              .map(function(s) {
+                var meta = s.sidebarMeta;
+                return {
+                  name: s.name,
+                  title: s.title,
+                  level: (s.name.match(/\./g) || []).length,
+                  order: meta.order,
+                  icon: meta.icon,
+                  stateRef: s.name,
+                };
+              })
+              .sort(function(a, b) {
+                return (a.level - b.level) * 100 + a.order - b.order;
+              });
+        }
+
+        function shouldMenuBeCollapsed() {
+          return window.innerWidth <= layoutSizes.resWidthCollapseSidebar;
+        }
+
+        function canSidebarBeHidden() {
+          return window.innerWidth <= layoutSizes.resWidthHideSidebar;
+        }
+      }
+
+    };
+
   }
 })();
