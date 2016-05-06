edb7f3975bc4e200066d0da2d54dcae4f9a08d3e
KostyaDanovsky <kostya.danovsky@gmail.com>
2016-05-06 20:12:19 +0300

    cleanup(dashboard): remove unused code

--- src/app/pages/dashboard/trafficChart/TrafficChartCtrl.js
+++ src/app/pages/dashboard/trafficChart/TrafficChartCtrl.js
@@ -50,39 +50,6 @@
       },
     ];
 
-    /*
-    *       {
-     value: 2000,
-     color: layoutColors.defaultCharts,
-     highlight: "#f1f1f1",
-     label: 'Ad Campaigns'
-     },
-     {
-     value: 1500,
-     color: layoutColors.primaryCharts,
-     highlight: "#136775",
-     label: 'Search engines'
-     },
-     {
-     value: 1000,
-     color: layoutColors.infoCharts,
-     highlight: "#13998a",
-     label: 'Direct Traffic'
-     },
-     {
-     value: 1200,
-     color: layoutColors.successCharts,
-     highlight: "#94d7a1",
-     label: 'Referral Traffic'
-     },
-     {
-     value: 400,
-     color: layoutColors.warningCharts,
-     highlight: "#dcfcc4",
-     label: 'Other'
-     }
-    * */
-
     var ctx = document.getElementById('chart-area').getContext('2d');
     window.myDoughnut = new Chart(ctx).Doughnut($scope.doughnutData, {
       segmentShowStroke: false,
