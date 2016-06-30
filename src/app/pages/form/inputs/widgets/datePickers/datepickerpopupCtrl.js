37a748cbcc2f85e83211f0277b2a6721c2a54d29
tibing <parapupum@gmail.com>
2016-06-30 11:25:49 +0300

    feat(app\form inputs): add datepicker

---
+++ src/app/pages/form/inputs/widgets/datePickers/datepickerpopupCtrl.js
@@ -0,0 +1,26 @@
+/**
+ * Created by n.poltoratsky
+ * on 23.06.2016.
+ */
+(function(){
+    'use strict';
+
+    angular.module('BlurAdmin.pages.form')
+        .controller('datepickerpopupCtrl', datepickerpopupCtrl);
+
+    /** @ngInject */
+    function datepickerpopupCtrl($scope) {
+
+        $scope.open = open;
+        $scope.opened = false;
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+        $scope.options = {
+            showWeeks: false
+        };
+
+        function open() {
+            $scope.opened = true;
+        }
+    }
+})();
